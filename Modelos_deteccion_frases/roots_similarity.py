5eaaf01ec970b056f407587f8707c28b4bb7ac45
Ale-Mildiner <alemildiner26@gmail.com>
2023-05-24 14:35:30 -0300

    mod en root similarity

--- Modelos_deteccion_frases/roots_similarity.py
+++ Modelos_deteccion_frases/roots_similarity.py
@@ -2,7 +2,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import pickle
 
-path = 'c:/Facultad/Tesis/'
+path = ''
 root_emb = pickle.load(open(path +'root_emb.pickle', 'rb'))
 similarity = cosine_similarity(np.array(root_emb))
-pickle.dump(similarity, open(path+'root_similarity.pickle', 'wb'))
+np.fill_diagonal(similarity, 0)
+filas, columnas = np.where(similarity >0.5)
+
+pickle.dump(filas, open(path+'filas_index_05.pickle', 'wb'))
+pickle.dump(columnas, open(path+'columnas_index_05.pickle', 'wb'))
+
+#pickle.dump(similarity, open(path+'root_similarity.pickle', 'wb'))

422ed7bd9100f1f481755c299270e3b3ad6b5106
unknown <alemildiner26@gmail.com>
2023-06-20 21:44:56 -0300

    cambios en rand_index

--- Rand_index/rand_index.py
+++ Rand_index/rand_index.py
@@ -4,7 +4,9 @@ import pandas as pd
 import pickle as pk
 import itertools
 import requests
+import time
 
+t0 = time.time()
 def calculate_rand_index(partitionA, partitionB):
     n = len(partitionA)
     a = 0  # Pares concordantes en la misma partición en ambas agrupaciones
@@ -23,14 +25,14 @@ def calculate_rand_index(partitionA, partitionB):
     rand_index = (a + b) / num_combinations
     return rand_index
 
-pathGit = "c:/Git_proyects/Tesis/Embbedings_Lkvec/"
-path = "c:/Facultad/Tesis/"
+pathGit = "d:/Git_proyects/Tesis/Embbedings_Lkvec/"
+path = "d:/Facultad/Tesis/"
 Lkvec = pd.read_csv(path+'Lkvec_all_in.csv')
 Lkvec_unique = Lkvec.drop_duplicates(subset=['phrase'])
 
 c_mnet = pk.load(open(pathGit+'clusters_threshold_75_22.pk', 'rb'))
 c_mini = pk.load(open(pathGit+'clusters_threshold_75_02.pk', 'rb'))
-phr = pk.load(open(path+'phrases_to_emb.pickle', 'rb'))
+phr = pk.load(open(path+'phr_embbedings/phrases_to_emb.pickle', 'rb'))
 
 
 cluster_mapping_mnet = {phr[j]: str(i+1) for i, clus in enumerate(c_mnet) for j in clus}
@@ -46,10 +48,13 @@ Clusters_ids_mini = np.array(Lkvec_unique['id_cluster_mini'])
 rand_index_mini = calculate_rand_index(Lkvec_ids, Clusters_ids_mini)
 rand_index_mnet = calculate_rand_index(Lkvec_ids, Clusters_ids_mnet)
 
-print('mnet', rand_index_mnet)
-print('mini ',rand_index_mini)
+print('mnet', rand_index_mnet, '\n')
+print('mini ',rand_index_mini, '\n')
+
+tf = time.time()
+print('tiempo', tf-t0)
 TOKEN = "6287446315:AAFAnvbB6vUSzttp-smI5E00jDP7hNI7kCo" 
 chat_id = ""
-message = f"Termine con los siguientes valores \n mini = {rand_index_mini} \n mpnet = {rand_index_mnet}"
+message = f"Termine con los siguientes valores \n mini = {rand_index_mini} \n mpnet = {rand_index_mnet} \n tiempo = {tf-t0}"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 print(requests.get(url).json()) # this sends the message     
